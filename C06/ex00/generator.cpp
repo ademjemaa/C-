4137fec824fc1e2767155742b33daf4112522564
Adam JEMAA <adjemaa@e1r11p4.42.fr>
2020-10-24 17:56:04 +0200

    give me money

--- C06/ex00/generator.cpp
+++ C06/ex00/generator.cpp
@@ -1,9 +1,17 @@
 #include "generator.hpp"
 
-Generator::Generator(std::string const original)
+Generator::Generator(std::string const original) : original(original)
 {
-    this->original = original;
     for (int i; i < 4; i++)
         this->types[i] = 0;
     this->convert();
+}
+
+~Generator::Generator(void)
+{
+
+}
+
+Generator::Generator(Generator const &gen) : original(gen.original)
+{
 }
